d41ce1a3d670a6f53278488e99a012d3b280ad8e
blandskron <bastianlandskronfreelancer@gmail.com>
2025-01-14 20:14:06 -0300

    bootstrap

---
+++ link/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Link(models.Model):
+    url = models.URLField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.url
+    
